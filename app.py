63e2e910c362671da0a7b3652890cc76175fb9be
gantra747-png <gantra747@gmail.com>
2025-12-28 15:54:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
 
 st.title('🏠 House Price prediction using ML')
 st.image('https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif')
@@ -22,5 +23,15 @@ for i in X:
   all_value.append(ans)
 
 # st.write(all_value)
-  
 
+scaler = StandardScaler()
+scaled_X = scaler.fit_transform(X)  
+final_value = scaler.transform([all_value])
+
+model = RandomForestRegressor()
+model.fit(X,y)
+house_price = model.predict(final_value)
+
+with st.spinner('Predicting House price'):
+  time.sleep(3)
+st.write(house_price)
