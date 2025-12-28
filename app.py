bcc8439205a6d610fdc364f46ab3f266badaaa1e
gantra747-png <gantra747@gmail.com>
2025-12-28 15:38:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,11 @@ st.sidebar.title('🏠 Select House Features')
 st.sidebar.image('https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif')
 all_value = []
 for i in X:
-  ans = st.sidebar.slider(f'Select {i} value')
+  min_value = int(X[i].min())
+  max_value = int(X[i].max())
+  ans = st.sidebar.slider(f'Select {i} value',min_value, max_value)
   all_value.append(ans)
 
-st.write(all_value)
+# st.write(all_value)
   
+
