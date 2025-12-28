9563a8ced21f5fa0cca62395928a8fce9b0d94e6
gantra747-png <gantra747@gmail.com>
2025-12-28 15:14:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
 
-st.title('🏠House Price prediction using ML')
+st.title('🏠 House Price prediction using ML')
 st.image('https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif')
 
+df = pd.read_csv('house_data.csv')
+X = df.iloc[:,:-3]
+y = df.iloc[:,-1]
+
+st.sidebar.title('🏠 Select House Features')
+st.sidebar.image('https://i.pinimg.com/originals/93/c7/44/93c744bcde1780c94bb1d3f03991f8a7.gif')
+all_value = []
+for i in X:
+  ans = st.sidebar.slider(f'Select {i} value')
+  all_value.append(ans)
+
+st.write(all_value)
+  
