cfb88e95947aba43f81585cf4ebe54977d34a366
gantra747-png <gantra747@gmail.com>
2025-12-28 16:11:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ final_value = scaler.transform([all_value])
 
 model = RandomForestRegressor()
 model.fit(X,y)
-house_price = model.predict(final_value)[0][0]
+house_price = model.predict(final_value)[0]
 
 with st.spinner('Predicting House price'):
   time.sleep(1)
@@ -38,5 +38,6 @@ msg = f'''House price is: $ {house_price*100000}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Antra Gupta**''')
+
 
 
