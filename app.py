cb65e889c4702f43bec78cd2f0e62ee14a40c9fa
gantra747-png <gantra747@gmail.com>
2025-12-28 16:35:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,13 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)  
 final_value = scaler.transform([all_value])
 
-model = st.cache(RandomForestRegressor)()
-(model.fit(X,y)
+@st.cache_data
+def model_run(X,y):
+  model =RandomForestRegressor()
+  model.fit(X,y)
+  return model
+
+model = model_run(X,y)
 house_price = model.predict(final_value)[0]
 
 with st.spinner('Predicting House price'):
@@ -38,6 +43,7 @@ msg = f'''House price is: $ {round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Antra Gupta**''')
+
 
 
 
